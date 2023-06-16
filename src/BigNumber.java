a80a44395f15a03f8c984b37e707d864f62c961e
Wendel Tonzar <wendeltonzar@gmail.com>
2023-06-15 22:36:42 -0300

    Day four.

---
+++ src/BigNumber.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class BigNumber {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int firstNumber = scanner.nextInt();
+        
+        System.out.print("Enter the second number: ");
+        int secondNumber = scanner.nextInt();
+
+        if (firstNumber > secondNumber) {
+            System.out.println(firstNumber);
+        } else {
+            System.out.println(secondNumber);
+        }
+
+    }
+}
+
+
+
+

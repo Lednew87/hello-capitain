5c20916f67ea32efa77938e3e993cc38622d6927
Wendel Tonzar <wendeltonzar@gmail.com>
2023-06-15 21:58:17 -0300

    Boxed and Unboxed types.

---
+++ src/NameYear.java
@@ -0,0 +1,9 @@
+public class NameYear {
+
+    public static void main(String[] args) {
+        int year = 20;
+        String name = "Lednew Raznot";
+
+        System.out.println("Hello "+ name + "! Do you have "+ year + " years old!");
+    }
+}
